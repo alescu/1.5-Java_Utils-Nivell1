01acac34b1ca0e8d67ee073cbfc4af6df5efdd24
Georgy Voskanyan <hello@voskanyan.dev>
2025-01-27 13:29:54 +0100

    Eliminats els class i modificació del Main i la estructura de projecte per exercici.

---
+++ src/Main_Exercici_1.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main_Exercici_1 {
+    public static void main(String[] args){
+
+        // Exercici 1 : Llegir carpeta
+        if (args.length == 0) {
+            System.out.println("Error: Cal especificar un camí de directori");
+            System.out.println("Ús: java main.java <camí_del_directori>");
+            return;
+        }
+
+        File file = new File(args[0]);
+
+        File[] arrFile = null;
+        if(file.exists() && file.isDirectory()){
+            arrFile = file.listFiles();
+        }
+
+        Arrays.sort(arrFile, Comparator.comparing(File::getName, String.CASE_INSENSITIVE_ORDER));
+
+        for (File fitxer : arrFile) {
+            System.out.println("  " + (fitxer.isDirectory() ? "[D] " : "[F] ") + fitxer.getName());
+        }
+
+
+        // Exercici 2 : Afegeix a la classe de l’exercici anterior, la funcionalitat de llistar un arbre de directoris amb el contingut de tots els seus nivells (recursivament) de manera que s'imprimeixin en pantalla en ordre alfabètic dins de cada nivell, indicant a més si és un directori (D) o un fitxer (F), i la seva última data de modificació.
+
+
+    }
+}
--- src/Main_Exercici_2.java
+++ src/Main_Exercici_2.java
@@ -1,18 +1,11 @@
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
 
-public class Main {
+public class Main_Exercici_2 {
     public static void main(String[] args){
 
-
-        // javac ./src/main.java
-        // java ./src/main.java
-        // java ./src/main.java c:/
-
-        // Exercici 1
+        // Exercici 1 : Llegir carpeta
         if (args.length == 0) {
             System.out.println("Error: Cal especificar un camí de directori");
             System.out.println("Ús: java main.java <camí_del_directori>");
@@ -33,7 +26,7 @@ public class Main {
         }
 
 
-        // Exercici 2
+        // Exercici 2 : Afegeix a la classe de l’exercici anterior, la funcionalitat de llistar un arbre de directoris amb el contingut de tots els seus nivells (recursivament) de manera que s'imprimeixin en pantalla en ordre alfabètic dins de cada nivell, indicant a més si és un directori (D) o un fitxer (F), i la seva última data de modificació.
 
 
     }
